b96085fe1ef911c58778d5a1f580532a66606de1
Martin Brochhaus <mbrochh@gmail.com>
2012-12-18 15:37:39 +0800

    Sending the channel name with the message.
    It seems as if django-socketio is not really using the rooms feature of
    socket.io. Need to look into this again later, for now it works fine.

--- messenger/views.py
+++ messenger/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 
+from django.views.decorators.csrf import csrf_exempt
 from django_socketio import broadcast_channel as socketio_broadcast_channel
 
 
+@csrf_exempt
 def broadcast_channel(request, *args, **kwargs):
+    """
+    API wrapper around ``broadcast_channel`` method of ``django-socketio``.
+
+    """
     channel = request.POST.get('channel')
     message = request.POST.get('message')
+    message = '{0}: {1}'.format(channel, message)
     socketio_broadcast_channel(message, channel)
     return HttpResponse
