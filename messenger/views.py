989a9b7d9a5d3b5073381ca7d22627f52f79f24f
Martin Brochhaus <mbrochh@gmail.com>
2013-01-02 21:38:28 +0800

    Catching NoSocket error.
    This one happens, when no client subscribed to the given channel, which is OK.

--- messenger/views.py
+++ messenger/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from django.views.decorators.csrf import csrf_exempt
 from django_socketio import broadcast_channel as socketio_broadcast_channel
+from django_socketio import NoSocket
 
 
 @csrf_exempt
@@ -13,5 +14,8 @@ def broadcast_channel(request, *args, **kwargs):
     channel = request.POST.get('channel')
     message = request.POST.get('message')
     message = '{0}: {1}'.format(channel, message)
-    socketio_broadcast_channel(message, channel)
+    try:
+        socketio_broadcast_channel(message, channel)
+    except NoSocket:
+        pass
     return HttpResponse
